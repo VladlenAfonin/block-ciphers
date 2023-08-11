b9d15e46578dbd1fa29c262ec6c1bd7ce210b346
unknown <alyonka.mukhortova@gmail.com>
2023-08-12 00:18:36 +0200

    Add pt

--- main.py
+++ main.py
@@ -17,8 +17,10 @@ def main():
     target_key = np.array([4, 5, 4, 1, 2, 15, 0, 12])
     target_plaintext = np.array([1, 2, 3, 4])
     target_plaintext2 = np.array([2, 3, 4, 5])
+    target_plaintext3 = np.array([0, 0, 0, 0])
     target_ciphertext2 = np.array([3, 6, 3, 0])
     target_ciphertext = np.array([7, 7, 8, 9])
+    target_ciphertext3 = simple.encrypt(target_plaintext3, target_key)
 
     # c1 = common.split_int(simple.encrypt_rounds(target_plaintext, [target_key[:4]], 1), 4, 16)
 
@@ -41,7 +43,8 @@ def main():
 
         master_key = np.hstack([common.split_int(s[pt], 4, key_size_half), key_array])
         if np.all(simple.encrypt(target_plaintext2, master_key) == target_ciphertext2):
-            print(master_key)
+            if np.all(simple.encrypt(target_plaintext3, master_key) == target_ciphertext3):
+                print(master_key)
 
 
 if __name__ == '__main__':
